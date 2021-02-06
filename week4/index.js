e9d2013f36436f23254bee1ebb37448e48cf13cb
michelleucf <67243877+michelleucf@users.noreply.github.com>
2021-02-05 23:00:31 -0500

    Finished

--- week4/index.js
+++ week4/index.js
@@ -13,7 +13,7 @@ const s = new Sidebar({
 
 console.log(s.render() );
 /* 
-  TODO: Use the render() method of Sidebar to show HTML response on console. Results should be wrapped in <ul></ul>
+  TODO:  Use the render() method of Sidebar to show HTML response on console. Results should be wrapped in <ul></ul>
   
   The final result should look like the following:
   <ul><li>Albert Eggstein</li><li>Attila the Hen</li><li>Attila the Hen</li><li>Dixie Chick</li><li>Gregory Peck</li><li>Mary Poopins</li></ul>
