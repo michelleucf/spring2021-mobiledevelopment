449590662efdd8755d39527263c2e07bbfe42df2
michelleucf <67243877+michelleucf@users.noreply.github.com>
2021-02-15 19:20:33 -0500

    WIP

---
+++ week6/src/App.js
@@ -0,0 +1,11 @@
+import Example from './components/example/index';
+
+function App() {
+  return (
+     <Example/>
+       
+  );
+}
+
+export default App;
+
---
+++ week6/src/components/index.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+class Example extends React.Component{
+render(){
+    return <p>My name is Michelle O'Reilly.</p>;
+    }
+}
+
+export default Example; 
